cfcd5ba1bcd826598c976ba0f0165783202df952
Chris Azuaje <christopherazuaje@gmail.com>
2023-08-07 16:39:08 -0400

    double to single quotes

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -12,24 +12,24 @@ export function HangmanWord({
   return (
     <div
       style={{
-        display: "flex",
-        gap: ".25em",
-        fontSize: "6rem",
-        fontWeight: "bold",
-        textTransform: "uppercase",
-        fontFamily: "monospace",
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
       }}
     >
-      {wordToGuess.split("").map((letter, index) => (
-        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+      {wordToGuess.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }} key={index}>
           <span
             style={{
               visibility:
                 guessedLetters.includes(letter) || reveal
-                  ? "visible"
-                  : "hidden",
+                  ? 'visible'
+                  : 'hidden',
               color:
-                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+                !guessedLetters.includes(letter) && reveal ? 'red' : 'black',
             }}
           >
             {letter}
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,32 +1,32 @@
-import styles from "./Keyboard.module.css";
+import styles from './Keyboard.module.css';
 
 const KEYS = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
 ];
 
 type KeyboardProps = {
@@ -45,9 +45,9 @@ export function Keyboard({
   return (
     <div
       style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
-        gap: ".5rem",
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
+        gap: '.5rem',
       }}
     >
       {KEYS.map((key) => {
@@ -56,8 +56,8 @@ export function Keyboard({
         return (
           <button
             onClick={() => addGuessedLetter(key)}
-            className={`${styles.btn} ${isActive ? styles.active : ""} ${
-              isInactive ? styles.inactive : ""
+            className={`${styles.btn} ${isActive ? styles.active : ''} ${
+              isInactive ? styles.inactive : ''
             }`}
             disabled={isInactive || isActive || disabled}
             key={key}
